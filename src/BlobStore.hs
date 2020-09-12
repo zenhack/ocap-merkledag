c4792a333a2ae283774ee7b00f041553f8495c31
Ian Denhardt <ian@zenhack.net>
2020-09-12 15:28:54 -0400

    Big pile of comments.

--- src/BlobStore.hs
+++ src/BlobStore.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
+-- | This module defines the storage-independent parts of a blob store.
 module BlobStore
     ( KnownHash(..)
     , IllegalHash(..)
@@ -22,29 +23,43 @@ import qualified Data.ByteArray         as BA
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Lazy   as LBS
 
+-- | A hash we understand. This is morally the same as 'Hash' from the
+-- schema, except:
+--
+-- * It does not have an unknown variant
+-- * The diges is known to be the correct length for the algorithm.
 data KnownHash = Sha256 (Digest SHA256)
 
+-- | An exception indicating that a 'Hash' is not a valid 'KnownHash'.
 data IllegalHash
-    = UnsupportedAlgo HashAlgo
-    | WrongDigestLen
+    = UnsupportedAlgo HashAlgo -- ^ Unsupported hash algorithm.
+    | WrongDigestLen -- ^ Digest length is wrong for the specified algorithm.
     deriving(Show)
 instance Exception IllegalHash
 
+-- | A raw store for bytes. Supports setting and getting blob contents by hash,
+-- but does not compute or validate the hashes itself. You will want to wrap
+-- this in a 'BlobStore' via 'fromRaw' before using it.
 data RawBlobStore m = RawBlobStore
     { getBlobRaw :: KnownHash -> m BS.ByteString
     , putBlobRaw :: KnownHash -> LBS.ByteString -> m ()
     }
 
+-- | A blob store which validates its contents.
 newtype BlobStore m = BlobStore { rawStore :: RawBlobStore m }
 
+-- | Wrap a 'RawBlobStore' into a 'BlobStore'.
 fromRaw :: RawBlobStore m -> BlobStore m
 fromRaw = BlobStore
 
+-- | Fetch a blob from the store.
 getBlob :: MonadThrow m => BlobStore m -> Hash -> m StoredBlob
 getBlob bs hash = do
     h <- require $ decodeHash hash
+    -- TODO: add an integrity check.
     getBlobRaw (rawStore bs) h >>= Capnp.bsToValue
 
+-- | Add a blob to the store, canonicalizing it and returning the resulting hash.
 putBlob :: MonadThrow m => BlobStore m -> StoredBlob -> m Hash
 putBlob bs blob = do
     -- TODO: canonicalize (needs support in haskell-capnp)
@@ -54,10 +69,12 @@ putBlob bs blob = do
     putBlobRaw (rawStore bs) digest bytes
     pure $ encodeHash digest
 
+-- | Misc. helper function: unwrap an 'Either' and throw an exception on 'Left'.
 require :: (MonadThrow m, Exception e) => Either e a -> m a
 require (Left e)  = throw e
 require (Right v) = pure v
 
+-- | Decode a protocol hash into a guaranteed valid one.
 decodeHash :: Hash -> Either IllegalHash KnownHash
 decodeHash Hash{digest, algo} = case algo of
     HashAlgo'unknown' _ -> Left $ UnsupportedAlgo algo
@@ -65,11 +82,13 @@ decodeHash Hash{digest, algo} = case algo of
         Just d  -> Right $ Sha256 d
         Nothing -> Left WrongDigestLen
 
+-- | Encode a hash for use in the protocol.
 encodeHash :: KnownHash -> Hash
 encodeHash (Sha256 digest) = Hash
     { algo = HashAlgo'sha256
     , digest = BA.convert digest
     }
 
+-- | Compute the hash of a lazy bytestring.
 computeHash :: LBS.ByteString -> KnownHash
 computeHash bytes = Sha256 $ CH.hashlazy bytes
--- src/BlobStore/Files.hs
+++ src/BlobStore/Files.hs
@@ -1,8 +1,21 @@
 {-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+-- | This module implements a 'RawBlobStore' on top of a local filesystem.
 module BlobStore.Files
     ( open
     ) where
+
+-- The format of the store is a directory tree with the following structure:
+--
+-- * `tmp/` - empty at rest but used for temporary scratch space.
+-- * `blobs/` - stores the actual on-disk blobs
+--   * `sha256/` - blobs stored with a sha256 digest (currently the only
+--     supported hash). Within this there are 256 directores named `00`
+--     to `ff`, where each corresponds to the first byte of the hash.
+--     each of those has another 256 directories with the same name
+--     corresponding to the second byte of the hash, and each of those
+--     contains regular files with the actual contents of the blobs,
+--     named with the remainder of the hash in hexidecimal.
 
 import Zhp
 
@@ -24,6 +37,8 @@ import qualified Crypto.Hash as CH
 
 newtype FilesBlobStore = FilesBlobStore { storePath :: FilePath }
 
+-- | Open a raw blob store stored at the given path, initializing it if
+-- necessary.
 open :: FilePath -> IO (RawBlobStore IO)
 open storePath = do
     let fbs = FilesBlobStore{storePath}
@@ -33,6 +48,8 @@ open storePath = do
         , putBlobRaw = filesPutRaw fbs
         }
 
+-- | Initialize the store (creating the directory structure
+-- described above).
 initStore :: FilesBlobStore -> IO ()
 initStore FilesBlobStore{storePath} = do
     let mkdirP = createDirectoryIfMissing True
@@ -41,18 +58,22 @@ initStore FilesBlobStore{storePath} = do
         for_ [0..0xff] $ \(b :: Word8) ->
             mkdirP $ printf "%s/sha256/%02x/%02x" storePath a b
 
+-- | Get a blob from the store.
 filesGetRaw :: FilesBlobStore -> KnownHash -> IO BS.ByteString
 filesGetRaw fbs hash =
     BS.readFile (hashPath fbs hash)
 
+-- | Put a blob into the store.
 filesPutRaw :: FilesBlobStore -> KnownHash -> LBS.ByteString -> IO ()
 filesPutRaw fbs hash bytes =
     -- FIXME: do this atomically:
     LBS.writeFile (hashPath fbs hash) bytes
 
+-- | Compute the path in which to store a blob with the given digest.
 hashPath :: FilesBlobStore -> KnownHash -> FilePath
 hashPath fbs (Sha256 d) = sha256Path fbs d
 
+-- | Compute the path in which to store a blob with the given sha256 digest.
 sha256Path :: FilesBlobStore -> Digest SHA256 -> FilePath
 sha256Path FilesBlobStore{storePath} digest =
     printf "%s/blobs/sha256/%02x/%02x/%s"
