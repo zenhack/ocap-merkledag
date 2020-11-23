d5b551335499ffa46b9410eb90bd704de3af14c9
Ian Denhardt <ian@zenhack.net>
2020-11-22 23:13:40 -0500

    Wire up client to the cli.
    Need to implement putBytesStreaming on the server side before this can
    be used.

--- src/LibMain.hs
+++ src/LibMain.hs
@@ -4,23 +4,28 @@ module LibMain (main) where
 
 import Zhp
 
-import Network.Simple.TCP (HostPreference(Host), ServiceName, serve)
+import Network.Simple.TCP (HostPreference(Host), ServiceName, connect, serve)
 
 import BlobStore       (fromRaw)
 import BlobStore.Files (open)
+import Client          (storeFileRef)
 import Server          (StoreServer (..))
 
 import Capnp.Gen.Protocol.Pure
 
-import Capnp       (def, defaultLimit)
-import Capnp.Rpc   (ConnConfig (..), handleConn, socketTransport, toClient)
+import Capnp     (def, defaultLimit)
+import Capnp.Rpc
+    (ConnConfig (..), fromClient, handleConn, socketTransport, toClient)
 import Supervisors (withSupervisor)
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        ["serve", path, host, port] -> server path (fromString host) (fromString port)
+        ["serve", path, host, port] ->
+            server path (fromString host) (fromString port)
+        ["put", host, port, path] ->
+            putFile host (fromString port) path
         _                  -> do
             hPutStrLn stderr "Usage: omd serve <path> <host> <port>"
             exitFailure
@@ -34,3 +39,11 @@ server path host port = do
             handleConn (socketTransport sock defaultLimit) def
                 { getBootstrap = \_ -> pure $ Just client
                 }
+
+putFile :: String -> ServiceName -> FilePath -> IO ()
+putFile host port path =
+    connect host port $ \(sock, _remoteAddr) ->
+        handleConn (socketTransport sock defaultLimit) def
+            { withBootstrap = Just $ \_sup store ->
+                storeFileRef path (fromClient store) >>= print
+            }
