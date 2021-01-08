6878d2cee33b3fd3d08ee913a4dcb0c6d87b32df
Ian Denhardt <ian@zenhack.net>
2021-01-08 01:10:14 -0500

    Fix remaining build errors in FileArena.
    This swaps out unix-bytestring for a new package, unix-simple, of my own
    creation. unix-bytestring has name collisions with the regular `unix`
    package that I don't want to fuss about solving, and it's missing some
    APIs I need anyway.

--- src/BlobStore/BigFile/FileArena.hs
+++ src/BlobStore/BigFile/FileArena.hs
@@ -1,21 +1,25 @@
+{-# LANGUAGE DataKinds      #-}
 {-# LANGUAGE NamedFieldPuns #-}
 module BlobStore.BigFile.FileArena
     ( FileArena
     , fromFd
+    , readMsg
+    , writeMsg
     ) where
 
 import Zhp hiding (length)
 
 import Capnp.Bits                 (ByteCount)
 import Capnp.Gen.DiskBigfile.Pure
-import Capnp.Message              (singleSegment)
-import Control.Concurrent.STM     (STM, TVar)
+import Control.Concurrent.STM
+import Control.Exception.Safe     (throwIO)
 import System.Posix.Types         (Fd, FileOffset)
 
 import qualified Capnp
-import qualified Data.ByteString            as BS
-import qualified Data.ByteString.Lazy       as LBS
-import qualified System.Posix.IO.ByteString as BIO
+import qualified Capnp.Message        as M
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Unix
 
 
 byteCountToOffset :: ByteCount -> FileOffset
@@ -28,42 +32,43 @@ data FileArena = FileArena
 
 fromFd :: Fd -> FileOffset -> IO FileArena
 fromFd fd off = do
-    nextAlloc <- newTVar off
+    nextAlloc <- newTVarIO off
     pure FileArena { fd, nextAlloc }
 
 alloc :: ByteCount -> FileArena -> STM FileOffset
 alloc count FileArena{nextAlloc} = do
     offset <- readTVar nextAlloc
-    writeTVar nextAlloc $! nextAlloc + count
+    writeTVar nextAlloc $! offset + fromIntegral count
     pure offset
 
 writeBS :: BS.ByteString -> FileArena -> IO FileOffset
 writeBS bs = writeLBS (LBS.fromStrict bs)
 
 writeLBS :: LBS.ByteString -> FileArena -> IO FileOffset
-writeLBS lbs arena = do
-    offset <- atomically $ alloc $ ByteCount $ LBS.length lbs
-    fdPwritev fd (LBS.toChunks lbs) offset
+writeLBS lbs arena@FileArena{fd} = do
+    offset <- atomically $ alloc (fromIntegral $ LBS.length lbs) arena
+    pwritev fd (LBS.toChunks lbs) offset
     pure offset
 
 -- TODO: replace this with a wrapper for the pwritev() syscall, maybe pushing one
 -- upstream.
-fdPwritev :: Fd -> [BS.ByteString] -> FileOffset -> IO ()
-fdPwritev fd chunks offset = case chunks of
+pwritev :: Fd -> [BS.ByteString] -> FileOffset -> IO ()
+pwritev fd chunks offset = case chunks of
     [] -> pure ()
     (c:cs) -> do
-        fdPwrite fd c offset
-        fdPwritev fd cs (ofsfet + BS.length c)
+        -- FIXME: somewhere we need to deal with short writes
+        Unix.pwrite fd c offset
+        pwritev fd cs (offset + fromIntegral (BS.length c))
 
 
 
 ----
 
 readMsg :: Addr a -> FileArena -> IO (Capnp.Message 'Capnp.Const)
-readMsg Addr{offset, length, flatMessage} = do
-    bytes <- BIO.fdPread length offset
+readMsg Addr{offset, length, flatMessage} FileArena{fd} = do
+    bytes <- Unix.preadExn fd (fromIntegral length) (fromIntegral offset)
     if flatMessage
-        then pure $ singleSegment $ Capnp.fromByteString bytes
+        then pure $ M.singleSegment $ Capnp.fromByteString bytes
         else Capnp.bsToMsg bytes
 
 writeMsg :: Capnp.Message 'Capnp.Const -> FileArena -> IO (Addr a)
@@ -71,7 +76,7 @@ writeMsg msg arena = do
     nsegs <- Capnp.numSegs msg
     if nsegs == 1
         then (do
-            seg <- getSegment msg 0
+            seg <- M.getSegment msg 0
             writeSingleSegment seg arena)
         else
             writeMultiSegment msg arena
@@ -80,10 +85,18 @@ writeSingleSegment :: Capnp.Segment 'Capnp.Const -> FileArena -> IO (Addr a)
 writeSingleSegment seg arena = do
     let bs = Capnp.toByteString seg
     offset <- writeBS bs arena
-    pure Addr {offset, length = BS.length bs, flatMessage = True}
+    pure Addr
+        { offset = fromIntegral offset
+        , length = fromIntegral $ BS.length bs
+        , flatMessage = True
+        }
 
 writeMultiSegment :: Capnp.Message 'Capnp.Const -> FileArena -> IO (Addr a)
 writeMultiSegment msg arena = do
     let lbs = Capnp.msgToLBS msg
     offset <- writeLBS lbs arena
-    pure Addr {offset, length = LBS.length lbs, flatMessage = False}
+    pure Addr
+        { offset = fromIntegral offset
+        , length = fromIntegral $ LBS.length lbs
+        , flatMessage = False
+        }
