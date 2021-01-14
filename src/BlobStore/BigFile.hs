58115e582ff6647cfc3d025342d21bc7c2817c12
Ian Denhardt <ian@zenhack.net>
2021-01-13 19:59:49 -0500

    A bit more work on integrating the BigFile backend.
    This is sloppy; I need to step back and think carefully about
    concurrency here.

--- src/BlobStore/BigFile.hs
+++ src/BlobStore/BigFile.hs
@@ -5,16 +5,19 @@ module BlobStore.BigFile
     ) where
 
 import           BlobStore
+import qualified BlobStore.BigFile.DiskTrie  as DiskTrie
 import qualified BlobStore.BigFile.FileArena as FA
 import qualified BlobStore.BigFile.MemTrie   as MemTrie
+import qualified BlobStore.BigFile.TrieKey   as Key
 import           Capnp
     (defaultLimit, evalLimitT, msgToValue)
 import           Capnp.Bits                  (ByteCount(..))
 import           Capnp.Gen.DiskBigfile.Pure
 import           Capnp.Gen.Storage.Pure
 import qualified Capnp.Untyped.Pure          as U
-import           Control.Concurrent.STM      (atomically)
+import           Control.Concurrent.STM
 import           Data.Acquire                (Acquire)
+import qualified Data.ByteArray              as BA
 import qualified Data.ByteString             as BS
 import qualified Data.Text                   as T
 import qualified Data.Vector                 as V
@@ -24,14 +27,11 @@ open :: FilePath -> StoreInfo -> Acquire Store
 open path StoreInfo{blobFile, mapFile=StoreInfo'mapFile'{arena, mapRoot}} = do
     blobArena <- openArena path blobFile
     spineArena <- openArena path arena
-    pure Store
-        { blobArena
-        , spineArena
-        , blobMap = BlobMap
-            { mem = MemTrie.empty
-            , disk = mapRoot
-            }
+    blobMap <- liftIO $ newTVarIO BlobMap
+        { mem = MemTrie.empty
+        , disk = mapRoot
         }
+    pure Store { blobArena, spineArena, blobMap }
 
 openArena :: FilePath -> Arena -> Acquire (FA.FileArena a)
 openArena rootPath Arena{path, size} =
@@ -40,34 +40,42 @@ openArena rootPath Arena{path, size} =
         (fromIntegral size)
 
 type Leaf = StoredBlob (Maybe (U.Ptr))
-type Branch = TriePtr Leaf
+type Branch = TrieBranch Leaf
 
 data Store = Store
     { blobArena  :: FA.FileArena Leaf
     , spineArena :: FA.FileArena Branch
-    , blobMap    :: BlobMap
+    , blobMap    :: TVar BlobMap
     }
 
 data BlobMap = BlobMap
-    { mem  :: MemTrie.MemTrie (Addr (Maybe U.Ptr))
-    , disk :: TriePtr (StoredBlob (Maybe U.Ptr))
+    { mem  :: MemTrie.MemTrie (Addr Leaf)
+    , disk :: TriePtr Leaf
     }
-{-
 
-findBlob :: Store -> KnownHash -> IO (Maybe (Addr a))
-findBlob = undefined
+findBlob :: Store -> KnownHash -> IO (Maybe (Addr Leaf))
+findBlob Store{spineArena, blobMap} (Sha256 h) = do
+    BlobMap{mem, disk} <- atomically $ readTVar blobMap
+    let key = case Key.makeKey (BA.convert h) of
+            Nothing -> error "impossible"
+            Just k  -> k
+    case MemTrie.lookup key mem of
+        Just addr -> pure $ Just addr
+        Nothing   -> DiskTrie.lookup key disk spineArena
 
 bigFilePutBlob :: Store -> KnownHash -> BS.ByteString -> IO ()
 bigFilePutBlob s h bytes = do
-    -- TODO: do we need to check for an existing blob first?
-    off <- FA.writeBS bytes (blobArena s)
-    let addr = Addr
-            { offset = fromIntegral off
-            , length = fromIntegral $ BS.length bytes
-            , flatMessage = True
-            }
-    atomically $ M.insert addr h (addrCache s)
-    pure ()
+    existing <- findBlob s h
+    case existing of
+        Just _ -> pure ()
+        Nothing -> do
+            off <- FA.writeBS bytes (blobArena s)
+            let addr = Addr
+                    { offset = fromIntegral off
+                    , length = fromIntegral $ BS.length bytes
+                    , flatMessage = True
+                    }
+            blobMapInsert (blobMap s) (spineArena s) addr
 
-trieInsert = undefined
--}
+blobMapInsert :: TVar BlobMap -> FA.FileArena Branch -> Addr Leaf -> IO ()
+blobMapInsert = undefined
