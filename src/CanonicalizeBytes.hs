992a7f0a716dd0389c62104fbc2c47d57a5b7358
Ian Denhardt <ian@zenhack.net>
2021-03-23 22:13:26 -0400

    Add perf hack for "canonicalizing" bytes.
    So we don't have to make an extra copy when the blob is a leaf of a
    BlobTree.
    
    The test currently fails (though it doesn't flag it as such via the exit
    code), need to fix.

---
+++ src/CanonicalizeBytes.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module CanonicalizeBytes
+    ( canonicalizeBytesBlob
+    ) where
+
+import           Capnp.Bits
+import qualified Capnp.Pointer           as P
+import qualified Data.ByteString         as BS
+import qualified Data.ByteString.Builder as BB
+import qualified Data.ByteString.Lazy    as LBS
+import           Zhp
+
+canonicalizeBytesBlob :: BS.ByteString -> LBS.ByteString
+canonicalizeBytesBlob bytes =
+    let nBytes :: ByteCount = fromIntegral (BS.length bytes)
+        nWords = bytesToWordsCeil nBytes
+        nPadBytes = wordsToBytes nWords - nBytes
+        segmentTotalWords = nWords + 4
+    in
+    BB.toLazyByteString $ mconcat
+        [ BB.word32LE 0 -- 1 segment
+        , BB.word32LE (fromIntegral segmentTotalWords) -- segment length
+        , ptr $ Just $ P.StructPtr 0 0 2 -- Root ptr
+        , ptr $ Just $ P.StructPtr 1 0 1 -- StoredBlob.data
+        , ptr $ Just $ P.ListPtr (fromIntegral $ nWords + 1) $ P.EltComposite 0 -- StoredBlob.ptrs
+        , ptr $ Just $ P.ListPtr 0 $ P.EltNormal P.Sz8 $ fromIntegral nBytes -- BlobTree.leaf
+        , BB.byteString bytes
+        , mconcat $ take (fromIntegral nPadBytes) $ repeat (BB.word8 0)
+        ]
+  where
+    ptr = BB.word64LE . P.serializePtr
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,35 @@
 module Main (main) where
 
-import Zhp
+import qualified CanonicalizeBytes
+import qualified Capnp
+import qualified Capnp.Canonicalize        as C
+import           Capnp.Classes             (toStruct)
+import           Capnp.Gen.Protocol.Pure
+import           Capnp.Gen.Storage.Pure
+import qualified Capnp.Message             as M
+import qualified Data.ByteString           as BS
+import qualified Data.Vector               as V
+import           Test.QuickCheck           (quickCheck)
+import           Test.QuickCheck.Instances ()
+import           Zhp
+
+prop_canonicalizeBytesEquiv :: BS.ByteString -> Bool
+prop_canonicalizeBytesEquiv bytes =
+    let value = StoredBlob
+            { data_ = BlobTree'leaf bytes
+            , ptrs = V.empty
+            }
+        Just normalCanonicalized =
+            Capnp.msgToLBS <$> Capnp.createPure maxBound (do
+                msg <- M.newMessage Nothing
+                structIn <- Capnp.cerialize msg value
+                (msg', _seg') <- C.canonicalize (toStruct structIn)
+                pure msg')
+        fastCanonicalized = CanonicalizeBytes.canonicalizeBytesBlob bytes
+    in
+    normalCanonicalized == fastCanonicalized
+
+
 
 main :: IO ()
-main = pure ()
+main = quickCheck prop_canonicalizeBytesEquiv
