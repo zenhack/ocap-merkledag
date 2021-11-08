d7e882c8b26d3468f9587d0ee914e84f7baaafb9
Ian Denhardt <ian@zenhack.net>
2021-11-08 02:19:15 -0500

    Add a pile of comments.

--- pkg/diskstore/filearena/filearena.go
+++ pkg/diskstore/filearena/filearena.go
@@ -6,11 +6,33 @@ import (
 	"sync"
 )
 
+// A FileArena represents an on-disk allocation arena.
+//
+// This takes the form of a flat file, where allocations
+// happen at the end of the file.
 type FileArena struct {
-	syncMu    *sync.RWMutex
-	allocMu   *sync.Mutex
-	file      *os.File
-	nextAlloc int64
+	file      *os.File // The underlying file
+	nextAlloc int64    // The next free offset into the file
+
+	// syncMu is a readers/writer lock, used as follows:
+	//
+	// - Sync() must hold the lock in write mode, to ensure that when
+	//   it measures the current offset, all space that has been allocated
+	//   has also been written to. It needn't hold the lock during the call
+	//   to the underlying file.Sync; that will just result in additional
+	//   data hitting the disk, which is fine.
+	// - Writers must hold the lock in _read_ mode when writing, somewhat
+	//   counter-intuitively.
+	//
+	// This has the effect of excluding calls to sync whenever the offset
+	// includes uninitialized storage, while allowing writes after the
+	// synchronization point to proceed without waiting on the fsync call.
+	syncMu *sync.RWMutex
+
+	// allocMu must be held when accessing nextAlloc, unless syncMu is
+	// held in write mod. syncMu must be acquired in read mode before.
+	// taking this lock.
+	allocMu *sync.Mutex
 }
 
 type Addr struct {
@@ -18,6 +40,8 @@ type Addr struct {
 	Size   uint32
 }
 
+// Construct a FileArnea from an open file and the next allocation offset into
+// that file.
 func New(file *os.File, nextAlloc int64) *FileArena {
 	return &FileArena{
 		syncMu:    &sync.RWMutex{},
@@ -27,6 +51,8 @@ func New(file *os.File, nextAlloc int64) *FileArena {
 	}
 }
 
+// Make changes to the arena durable. Returns the size of the arena at the time
+// when the snapshot was taken, and an error, if any.
 func (fa *FileArena) Sync() (off int64, err error) {
 	func() {
 		fa.syncMu.Lock()
@@ -37,6 +63,7 @@ func (fa *FileArena) Sync() (off int64, err error) {
 	return
 }
 
+// Write the bytes to the arena, returning its address.
 func (fa *FileArena) Put(data []byte) (addr Addr, err error) {
 	size := len(data)
 	fa.withAlloc(int64(size), func(off int64) {
@@ -49,6 +76,7 @@ func (fa *FileArena) Put(data []byte) (addr Addr, err error) {
 	return
 }
 
+// Return the bytes at the specified address.
 func (fa *FileArena) Get(addr Addr) (data []byte, err error) {
 	// TODO(perf): Maybe mmap if the blob is large enough.
 	data = make([]byte, addr.Size)
@@ -56,10 +84,16 @@ func (fa *FileArena) Get(addr Addr) (data []byte, err error) {
 	return
 }
 
+// Delete the data at the given address.
+//
+// We use spares files for this under the hood, so there appear to be "gaps"
+// in the file that are logically all zeros, but for which no storage is
+// allocated.
 func (fa *FileArena) Clear(addr Addr) error {
 	return fmt.Errorf("TODO")
 }
 
+// Like `Get`, but takes a user supplied buffer.
 func (fa *FileArena) ReadAt(b []byte, off int64) (n int, err error) {
 	return fa.file.ReadAt(b, off)
 }
