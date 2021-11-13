104e479f82620447585ef3a4e49e1f5a533d17b3
Ian Denhardt <ian@zenhack.net>
2021-11-13 01:26:11 -0500

    First pass on insert

--- pkg/diskstore/triemap/triemap.go
+++ pkg/diskstore/triemap/triemap.go
@@ -78,3 +78,116 @@ func lookup(s Storage, key []byte, m diskstore.TrieMap) (res diskstore.Addr, err
 		return res, ErrMalformed
 	}
 }
+
+type InsertResult struct {
+	ResAddr types.Addr
+	ResNode diskstore.TrieMap
+	OldAddr types.Addr
+	WasOld  bool
+}
+
+func insert(s Storage, key []byte, value diskstore.Addr, m diskstore.TrieMap) (res InsertResult, err error) {
+	if len(key) == 0 {
+		return saveLeaf(s, key, value)
+	}
+
+	switch m.Which() {
+	case diskstore.TrieMap_Which_leaf:
+		leaf := m.Leaf()
+		prefix, err := leaf.Prefix()
+		if err != nil {
+			return res, err
+		}
+		oldAddr, err := leaf.Addr()
+		if err != nil {
+			return res, err
+		}
+		if bytes.Compare(key, prefix) == 0 {
+			res, err = saveLeaf(s, key, value)
+			res.WasOld = true
+			res.OldAddr = types.DecodeAddr(oldAddr)
+			return res, err
+		} else {
+			res1, err := saveLeaf(s, key[1:], value)
+			if err != nil {
+				return res, err
+			}
+			res2, err := saveLeaf(s, prefix[1:], oldAddr)
+			if err != nil {
+				return res, err
+			}
+			_, seg, m := newNode()
+			branches, err := m.NewBranches(256)
+			if err != nil {
+				panic(err)
+			}
+			setBranch(branches, key[0], res1.ResAddr)
+			setBranch(branches, prefix[0], res2.ResAddr)
+			addr, err := s.Store(seg.Data())
+			return InsertResult{
+				ResAddr: addr,
+				ResNode: m,
+			}, err
+		}
+	case diskstore.TrieMap_Which_branches:
+		branches, err := m.Branches()
+		if err != nil {
+			return res, err
+		}
+		if branches.Len() != 256 {
+			return res, ErrMalformed
+		}
+		branchAddr := branches.At(int(key[0]))
+		data, err := s.Fetch(types.DecodeAddr(branchAddr))
+		if err != nil {
+			return res, err
+		}
+		msg := &capnp.Message{Arena: capnp.SingleSegment(data)}
+		mchild, err := diskstore.ReadRootTrieMap(msg)
+		if err != nil {
+			return res, err
+		}
+		res, err = insert(s, key[1:], value, mchild)
+		if err != nil {
+			return res, err
+		}
+		res.ResAddr.EncodeInto(branchAddr)
+		addr, err := s.Store(m.Struct.Segment().Data())
+		res.ResNode = m
+		res.ResAddr = addr
+		return res, err
+	default:
+		return res, ErrMalformed
+	}
+}
+
+func newNode() (*capnp.Message, *capnp.Segment, diskstore.TrieMap) {
+	msg := &capnp.Message{Arena: capnp.SingleSegment(nil)}
+
+	seg, err := msg.Segment(0)
+	if err != nil {
+		panic(err)
+	}
+	m, err := diskstore.NewRootTrieMap(seg)
+	if err != nil {
+		panic(err)
+	}
+	return msg, seg, m
+}
+
+func saveLeaf(s Storage, prefix []byte, value diskstore.Addr) (res InsertResult, err error) {
+	_, seg, root := newNode()
+	root.SetLeaf()
+	newLeaf := root.Leaf()
+	newLeaf.SetPrefix(prefix)
+	newLeaf.SetAddr(value)
+	addr, err := s.Store(seg.Data())
+	return InsertResult{
+		ResAddr: addr,
+		ResNode: root,
+	}, nil
+}
+
+func setBranch(list diskstore.Addr_List, index uint8, addr types.Addr) {
+	addr.EncodeInto(list.At(int(index)))
+}
--- pkg/diskstore/types/types.go
+++ pkg/diskstore/types/types.go
@@ -10,6 +10,12 @@ type Addr struct {
 	ArenaAddr filearena.Addr
 }
 
+func (a Addr) EncodeInto(da diskstore.Addr) {
+	da.SetArena(a.Arena)
+	da.SetOffset(uint64(a.ArenaAddr.Offset))
+	da.SetLength(a.ArenaAddr.Size)
+}
+
 func DecodeAddr(addr diskstore.Addr) Addr {
 	return Addr{
 		Arena: addr.Arena(),
